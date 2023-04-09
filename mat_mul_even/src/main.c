ecbfee4bc9c4195444e81374f0992f9c50e8bb29
RodTol <rodolfo.tolloi@gmail.com>
2023-04-09 10:53:18 +0200

    Sort of multiplication implemented

--- mat_mul_even/src/main.c
+++ mat_mul_even/src/main.c
@@ -7,11 +7,11 @@
 
 #define MASTER 0
 #define COMM MPI_COMM_WORLD
-#define N 6
+#define N 3
 
 int main(int argc, char** argv) {
     
-    double *A, *B, *C;
+    double *A, *B, *C, *B_loc;
 
     int n_proc_tot = 1, irank = 0;
     int n_loc = N, offset = 0, rest=0;
@@ -21,6 +21,8 @@ int main(int argc, char** argv) {
     MPI_Init ( & argc , & argv ) ;
     MPI_Comm_rank ( COMM , & irank ) ;
     MPI_Comm_size ( COMM , & n_proc_tot ) ;
+
+    MPI_Datatype Even_block;
 
     /*Parameters for the matrix distribution*/
     n_loc = N/n_proc_tot;
@@ -51,55 +53,78 @@ int main(int argc, char** argv) {
         #endif
     #endif
 
+    /*Allocation and initialisation*/
     int size= N * n_loc * sizeof( double);
     A = (double *) malloc( size );
-    array_of_random_doubles(A, N*n_loc);
+    array_of_random_doubles(A, n_loc*N);
 
     B = (double *) malloc( size );
-    create_identity_matrix_distributed(B, irank, n_loc, N,
-        offset, n_proc_tot);
+    //create_identity_matrix_distributed(B, irank, n_loc, N, offset, n_proc_tot);
+    array_of_random_doubles(B, n_loc*N);
 
     C = (double *) malloc( size );
-    create_null_array(C, N*n_loc);
+    create_null_array(C, n_loc*N);
 
-    /* 
-    #ifdef SMALL
-    MPI_Barrier(COMM);
-    printf("----@I am %d @----\n", irank );
-    print_matrix(A, n_loc, N);
-    printf("\n");
-    print_matrix(B, n_loc, N);
-    printf("\n");
-    #endif
+    /*The datatype that will represent the columns of B to be
+    * multiplied.
+    (#of tot elements, #taken from each proc, #distance from, Type, name)*/
+    MPI_Type_vector(n_loc, n_loc, N, MPI_DOUBLE, &Even_block);
+    MPI_Type_commit(&Even_block);
 
-    #ifdef SMALL
-    print_matrix(C, n_loc, N);
-    printf("\n");
-    #endif
-    */
+    /*Local buffer for the multiplication*/
+    B_loc = (double *) malloc( n_loc * N * sizeof(double) );
+    create_null_array(B_loc, n_loc*N);
+
+    /*The multiplication*/
+    for (int count = 0; count < n_proc_tot; count++) {
+        MPI_Allgather(&B[n_loc * count], 1, Even_block, B_loc, n_loc*n_loc, MPI_DOUBLE, COMM);
+
+        #ifdef SMALL
+            printf("\n----@ I am %d @----\n", irank);
+            printf("B_loc:  \n");
+            print_matrix(B_loc, N, n_loc);
+        #endif
+
+        for (int i = 0; i < n_loc; i++) { // A is n_loc x N
+            for (int j = 0; j < n_loc; j++) { // B_loc is N x n_loc
+                for (int k = 0; k < N; k++) { // C slice is n_loc x n_loc -> C is n_loc x N (same as A)
+                     C[i * N + j + count * n_loc] += A[i * N + k ] * B_loc[k * n_loc + j];
+                }
+            }
+        }
+
+        #ifdef SMALL
+        MPI_Barrier(COMM);
+        if (irank == MASTER) {
+            printf("\nMatrix C at count = %d \n", count);
+        }
+        print_matrix_distributed(C, irank, n_loc, N, n_proc_tot, COMM);
+        MPI_Barrier(COMM);
+        #endif
+    }
 
     /*If the matrices are small enough (N < 6 and n_proc < 4)
     the program will print the complete form*/
     #ifdef SMALL
         MPI_Barrier(COMM);
         if (irank == MASTER) {
-            printf("\n Matrix A \n");
+            printf("\nMatrix A \n");
         }
         print_matrix_distributed(A, irank, n_loc, N, n_proc_tot, COMM);
         
         if (irank == MASTER) {
-            printf("\n Matrix B \n");
+            printf("\nMatrix B \n");
         }
         print_matrix_distributed(B, irank, n_loc, N, n_proc_tot, COMM);
 
         if (irank == MASTER) {
-            printf("\n Matrix C \n");
+            printf("\nMatrix C \n");
         }
         print_matrix_distributed(C, irank, n_loc, N, n_proc_tot, COMM);
     #endif
 
     /*Final output and deallocation of the memory*/
-    free(A); free(B); free(C);
+    free(A); free(B); free(C), free(B_loc);
     MPI_Barrier(COMM);
     if (irank == MASTER) {
         printf("\n----@");
