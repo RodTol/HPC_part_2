1d0fcc62fd61283c8955431e9274a9e1c2e4229e
Rodolfo Tolloi <rodolfo.tolloi@gmail.com>
2023-05-01 19:20:41 +0200

    almost completed cublas dgemm

--- matrix_mult/src/headers/gpu_computation.h
+++ matrix_mult/src/headers/gpu_computation.h
@@ -2,6 +2,8 @@
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
 
-void allocation ();
+void initialise_cuda(double *A, double **dev_A, double **dev_B_col, double **dev_C,
+ int *n_rows_local, int N, int n_loc, int irank);
 
-void compute ();
+void computation(int count, double *B_col, double *dev_A, double *dev_B_col, double *dev_C,
+ int *n_row_local, int* displacement, int N, int n_loc, int irank, float *computation_Time, cublasHandle_t handle);
