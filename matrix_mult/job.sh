f783c8653d55f1c2ffd0a69548d1edfbf9e54357
RodTol <rodolfo.tolloi@gmail.com>
2023-05-07 17:35:07 +0200

    run8

--- matrix_mult/job.sh
+++ matrix_mult/job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -A tra23_units
 #SBATCH -p m100_usr_prod
-#SBATCH --time 01:00:00
-#SBATCH -N 8                  # nodes
+#SBATCH --time 03:00:00
+#SBATCH -N 1                  # nodes
 #SBATCH --ntasks-per-node=32
 #SBATCH --gres=gpu:4          # gpus per node out of 4
 #SBATCH --mem=246000          # memory per node out of 246000MB
@@ -20,7 +20,7 @@ export OMP_NUM_THREADS=1
 cd /m100/home/usertrain/a08trb39/HPC_part_2/matrix_mult
 
 n_proc=32
-((n_proc*=8))
+((n_proc*=1))
 
 for N in 1000 5000 10000 15000 20000 25000 30000
 do
