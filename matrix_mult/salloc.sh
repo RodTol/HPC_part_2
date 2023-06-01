d9f8ee09c2bf8a43455c405c02e800f083f687c5
RodTol <rodolfo.tolloi@gmail.com>
2023-06-01 17:17:08 +0200

    random

--- matrix_mult/salloc.sh
+++ matrix_mult/salloc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-salloc -A tra23_units -p m100_usr_prod --time=1:0:0 \
+salloc --exclusive -A tra23_units -p m100_usr_prod --time=1:0:0 \
  -N 2 \
 --ntasks-per-node=32 \
  --gres=gpu:4 --mem=246000 --ntasks-per-core=1 \
