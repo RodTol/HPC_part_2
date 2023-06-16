6e24852140ef0268572846cec6aec17dc2aa4dcb
Rodolfo Tolloi <rodolfo.tolloi@gmail.com>
2023-06-16 16:43:30 +0200

    job.sh

--- matrix_mult/job.sh
+++ matrix_mult/job.sh
@@ -21,13 +21,15 @@ cd /m100/home/usertrain/a08trb39/HPC_part_2/matrix_mult
 
 n_proc=32
 ((n_proc*=8))
+n_proc_gpu=4
+((n_proc_gpu*=8))
 
-for N in 1000 5000 10000 15000 20000 25000 30000 35000 40000
+for N in 1000 5000 10000 15000 20000 25000 30000 40000
 #for N in 30000
 do
-	make run N=$N CORES=$n_proc
-	make run N=$N CORES=$n_proc compilation=dgemm
-	make run N=$N CORES=$n_proc compilation=gpu
+	make run N=$N CORES=$n_proc n_socket=16 n_node=32
+	make run N=$N CORES=$n_proc n_socket=16 n_node=32 compilation=dgemm
+	make run N=$N CORES=$n_proc_gpu n_socket=2 n_node=4 compilation=gpu
 done
 
 echo "JOB FINISHED"
