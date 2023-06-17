ed71cc449b4f84cab2dcbe22572db76ec458c0d8
tra76 user <a08tra76@login01.m100.cineca.it>
2023-06-17 15:33:15 +0200

    runs2

--- matrix_mult/job.sh
+++ matrix_mult/job.sh
@@ -2,7 +2,7 @@
 #SBATCH -A tra23_units_bis
 #SBATCH -p m100_usr_prod
 #SBATCH --time 03:00:00
-#SBATCH -N 8                  # nodes
+#SBATCH -N 4                  # nodes
 #SBATCH --ntasks-per-node=32
 #SBATCH --gres=gpu:4          # gpus per node out of 4
 #SBATCH --mem=246000          # memory per node out of 246000MB
@@ -20,17 +20,22 @@ export OMP_NUM_THREADS=1
 cd /m100/home/usertrain/a08trb39/HPC_part_2/matrix_mult
 
 n_proc=32
-((n_proc*=8))
+((n_proc*=4))
 n_proc_gpu=4
-((n_proc_gpu*=8))
+((n_proc_gpu*=4))
+
 
 for N in 1000 5000 10000 15000 20000 30000 35000 40000 45000 50000
-#for N in 30000
 do
-	make run N=$N CORES=$n_proc n_socket=16 n_node=32
 	make run N=$N CORES=$n_proc n_socket=16 n_node=32 compilation=dgemm
 	make run N=$N CORES=$n_proc_gpu n_socket=2 n_node=4 compilation=gpu
 done
+
+for N in 1000 5000 10000 15000 20000 30000
+do
+	make run N=$N CORES=$n_proc n_socket=16 n_node=32
+done
+
 
 echo "JOB FINISHED"
 
