c83af9053b86f3a4200dec19cedee7fd5a25398b
Rodolfo Tolloi <rodolfo.tolloi@gmail.com>
2023-06-18 10:32:37 +0200

    jacobi comments openacc

--- jacobi_exercise/openACC_code/src/linearized_matrix_utility.c
+++ jacobi_exercise/openACC_code/src/linearized_matrix_utility.c
@@ -31,8 +31,9 @@ int linear_index ( int i, int j, int dim1, int dim2)
 }
 
 /**
- * @brief This function print a local matrix of dimension 
- * dim1 X dim2
+ * @brief This is a custom version of a function that prints a
+ * local matrix of dimension dim1 X dim2. I added some color
+ * to show more clearly the values of the ghosts layers
  * 
  * @param A matrix
  * @param dim_1 rows dimension
@@ -94,7 +95,7 @@ void print_matrix_distributed (double * A, int irank,
       /*This variable are only for irank==0
       so they can be moved inside the if*/
       double * A_tmp;
-      print_matrix ( A , dim_1[irank], dim_2, true);
+      print_matrix ( A , dim_1[irank], dim_2, true) ;
       if (divisor)     printf("-----------------------------------\n");
       for (int count = 1; count < n_proc_tot ; count ++ ) {
           int size= dim_1[count] * dim_2 * sizeof( double);
@@ -194,6 +195,16 @@ void create_identity_matrix_distributed (double * A, int irank,
   }
 }
 
+/**
+ * @brief Create the initial condition for the jacobi problem
+ * 
+ * @param A The matrix
+ * @param irank rank of the process
+ * @param dim_1 local number of rows of the matrix
+ * @param dim_2 local number of columns of the matrix
+ * @param offset offset for each process
+ * @param n_proc_tot total number of process
+ */
 void create_jacobi_start_distributed (double * A, int irank,
  int *dim_1 , int dim_2, int* offset, int n_proc_tot) {
   
@@ -211,26 +222,25 @@ void create_jacobi_start_distributed (double * A, int irank,
     }
   }
 
-  /*Right boundary is set to o*/
+  /*Right boundary is set to 0*/
   for (int i = 0; i < dim_1[irank]; i++) {
     A[linear_index(i,dim_2-1,dim_1[irank],dim_2)] = 0;
   }
   
+  /*Last row goes from 0 to 100 at regular steps*/
   double increment = 100.0 / (dim_2-1);
-  /*Last row*/
   if (irank==n_proc_tot-1) {
     for (int j = 0; j < dim_2; j++) {
       A[linear_index(dim_1[irank]-1,j,dim_1[irank],dim_2)] = (dim_2-j-1)*increment;
     }
   }
   
-  /*Left column*/
+  /*Left column goes from 0 to 100 at regular steps*/
   if (irank==0) {
     A[linear_index(0,0,dim_1[irank],dim_2)] = 0;
   }
   /*I don't need to touch the bottom left corner because
   is already setted up by the row initialisation*/
-
   for (int i = 1; i < dim_1[irank]-1; i++) {
     int displacement = offset[irank];
     if (irank!=0) {
@@ -239,15 +249,4 @@ void create_jacobi_start_distributed (double * A, int irank,
     A[linear_index(i,0,dim_1[irank],dim_2)] = (i+displacement)*increment;
   }
 
-}
-
-void matrix_multiplication(double* A, double* B_col, double* C, 
-  int N, int* n_rows_local, int* displacement, int irank, int count) {
-  for (int i = 0; i < n_rows_local[irank]; i++) {       // A is n_loc x N (i,j)
-      for (int k = 0; k < n_rows_local[count]; k++) {   // B_col is N x n_loc (j,k)
-        for (int j = 0; j < N; j++) {   // C slice is n_loc x n_loc -> C is n_loc x N (same as A)
-          C[i * N + k + displacement[count]] += A[i * N + j] * B_col[j* n_rows_local[count] + k];
-        }
-      }
-  }
 }
