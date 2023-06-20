3e937b116c429a134b485fe8846d7a94d6dafd86
tra76 user <a08tra76@login03.m100.cineca.it>
2023-06-20 14:04:41 +0200

    runs

--- matrix_mult/job.sh
+++ matrix_mult/job.sh
@@ -2,7 +2,7 @@
 #SBATCH -A tra23_units_bis
 #SBATCH -p m100_usr_prod
 #SBATCH --time 03:00:00
-#SBATCH -N 4                  # nodes
+#SBATCH -N 2                  # nodes
 #SBATCH --ntasks-per-node=32
 #SBATCH --gres=gpu:4          # gpus per node out of 4
 #SBATCH --mem=246000          # memory per node out of 246000MB
@@ -20,9 +20,9 @@ export OMP_NUM_THREADS=1
 cd /m100/home/usertrain/a08trb39/HPC_part_2/matrix_mult
 
 n_proc=32
-((n_proc*=4))
+((n_proc*=2))
 n_proc_gpu=4
-((n_proc_gpu*=4))
+((n_proc_gpu*=2))
 
 
 for N in 1000 5000 10000 15000 20000 30000 35000 40000 45000 50000
@@ -31,7 +31,7 @@ do
 	make run N=$N CORES=$n_proc_gpu n_socket=2 n_node=4 compilation=gpu
 done
 
-for N in 1000 5000 10000 15000 20000 30000
+for N in 1000 5000 10000 15000 20000
 do
 	make run N=$N CORES=$n_proc n_socket=16 n_node=32
 done
